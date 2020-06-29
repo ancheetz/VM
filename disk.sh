1d9c0e87ef3532b41430cf94361ac414daaade14
andreac <ancheetz@me.com>
2020-06-28 21:11:48 -0400

    bash system files

---
+++ disk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "WARNING! Disk Space Utilization has exceeded 10%."
+echo "This report was automatically generated on $(date)"
+echo "System details as follows: "
+echo "Disk Usage: $(df | awk 'FNR == 2 { print $3/$2*100 "% used " ($4/$2*100 "% free")}')"
+
+if [ $(df | awk 'FNR == 2 {$3/$2*100 -ge 10}') ]; then
+echo $(date)
+fi
---
+++ proc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello $USER, how are you today?"
+echo "Today is $(date)."
+echo "Your current system uptime is $(uptime)"
+echo 
