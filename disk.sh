169c580c9c493551910b8e0fd28ce25a90539a1a
andreac <ancheetz@me.com>
2020-06-29 23:21:18 -0400

    Backup script to use with cronjob; tar backup script added

--- disk.sh
+++ disk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+disk_usage()
+{
 echo "WARNING! Disk Space Utilization has exceeded 10%."
 echo "This report was automatically generated on $(date)"
 echo "System details as follows: "
@@ -7,3 +9,4 @@ echo "Disk Usage: $(df | awk 'FNR == 2 { print $3/$2*100 "% used " ($4/$2*100 "%
 if [ $(df | awk 'FNR == 2 {$3/$2*100 -ge 10}') ]; then
 echo $(date)
 fi
+}
