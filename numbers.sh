fc8da028d8b29f059ae08af618b66c4d0bf4f345
andreac <ancheetz@me.com>
2020-07-02 11:54:37 -0400

    working on for loops; committed scripts need work

--- numbers.sh
+++ numbers.sh
@@ -6,4 +6,11 @@ read -p "Enter a number: " num1
 read -p "Enter a second number: " num2
 if [ $num1 -lt 10 -o $num2 -lt 10 ];
 then
-for num in (0,9)
+for num in {0..9}
+do 
+echo "$num"
+exit1
+do
+let "sum=$num1+$num2"
+done
+echo "$sum"
