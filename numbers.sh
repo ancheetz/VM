7e06719d5563656cc4139274b9e03ba11ee5baa7
andreac <ancheetz@me.com>
2020-07-02 12:45:00 -0400

    for loop improved

--- numbers.sh
+++ numbers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #Loop with 2 numbers
 #If -lt 10, print all values; if -gt 10, add them and print sum
 
@@ -6,11 +6,10 @@ read -p "Enter a number: " num1
 read -p "Enter a second number: " num2
 if [ $num1 -lt 10 -o $num2 -lt 10 ];
 then
-for num in {0..9}
-do 
-echo "$num"
-exit1
-do
+for num in {0..9};
+do echo "$num";
+done;
+exit 1
 let "sum=$num1+$num2"
-done
 echo "$sum"
+fi
